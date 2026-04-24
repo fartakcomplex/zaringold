e0c5aed936191ddcbd717c1a03c80fcb885673d6
Z User <z@container>
2026-04-24 20:30:27 +0000

    bd83a5e3-cacb-4fc9-a12e-1d600ad60192

--- start-server.sh
+++ start-server.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 cd /home/z/my-project
-export NODE_OPTIONS="--max-old-space-size=512"
-
 while true; do
-  echo "[$(date)] Starting Next.js server..."
-  npx next dev --port 3000 >> dev.log 2>&1
-  EXIT_CODE=$?
-  echo "[$(date)] Server exited with code $EXIT_CODE, restarting in 3s..."
+  ./node_modules/.bin/next dev -p 3000 2>&1 | tee -a /home/z/my-project/dev.log
+  echo "Server crashed, restarting in 3s..."
   sleep 3
-  # Clean up stale .next cache on crash
-  rm -rf .next/cache 2>/dev/null
 done
